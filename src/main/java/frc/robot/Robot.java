a0a95c83e04f32ca6afebddb96b56362cd1d314d
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-02-19 16:46:45 +0300

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,55 +15,75 @@ public class Robot extends TimedRobot {
   private final WPI_TalonSRX kRightLeader  = new WPI_TalonSRX(7);
   private final WPI_TalonSRX kLeftMotor2 = new WPI_TalonSRX(6);
   private final WPI_TalonSRX kRightMotor2 = new WPI_TalonSRX(8);
-
-  private final CANSparkMax kShooter = new CANSparkMax(1, MotorType.kBrushed);
-  private final CANSparkMax kTopFeeder = new CANSparkMax(2, MotorType.kBrushless);
-  private final CANSparkMax kDownFeeder = new CANSparkMax(4, MotorType.kBrushed);
+ 
+  private final CANSparkMax armShoulder = new CANSparkMax(1, MotorType.kBrushed);
+  private final CANSparkMax armClamp = new CANSparkMax(2, MotorType.kBrushed);
+  private final CANSparkMax arm4 = new CANSparkMax(4, MotorType.kBrushed);
+  private final CANSparkMax arm3 = new CANSparkMax(3, MotorType.kBrushed);
 
   private final DifferentialDrive m_drive = new DifferentialDrive(kLeftLeader,kRightLeader);
-
+  
   private final Joystick joystick = new Joystick(0);
+
+  double auto_timer = 0;
 
   @Override
   public void robotInit() {
     kLeftMotor2.follow(kLeftLeader);
     kRightMotor2.follow(kRightLeader);
-
-    kShooter.setInverted(true);
-    kTopFeeder.setInverted(true);
   }
 
   @Override
-  public void robotPeriodic() {
-   
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {
+    auto_timer = Timer.getFPGATimestamp();
   }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousPeriodic() {
+    double curr = Timer.getFPGATimestamp();
+    m_drive.setSafetyEnabled(false);
 
-  @Override
-  public void autonomousPeriodic() {}
+    if (curr - auto_timer <= 3.5) {
+      m_drive.arcadeDrive(0, -0.63);
+    } else { 
+      m_drive.stopMotor();
+    }
+  }
 
   @Override
   public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {
-    m_drive.arcadeDrive(joystick.getRawAxis(2) / 1.5, joystick.getY());
-
+    m_drive.arcadeDrive(joystick.getRawAxis(2), -joystick.getY());
+    
+    
+    if (joystick.getRawButton(5)) {    
+      armShoulder.set(-0.7);
+      armClamp.set(1);
+    }
+    
+    else if (joystick.getRawButton(3)) {
+      armShoulder.set(0.3);
+     
+    } 
+    else {
+      armClamp.set(0);
+      armShoulder.set(0);
+    }
     if (joystick.getRawButton(1)) {
-      kShooter.set(1);
-    } else {
-      kShooter.set(0);
+      arm4.set(0.5);
     }
-
-    if (joystick.getRawButton(2)) {
-      kTopFeeder.set(0.35);
-      kDownFeeder.set(1);
+    else if(joystick.getRawButton(2)){
+      arm4.set(-0.8);
+     
     } else {
-      kTopFeeder.set(0);
-      kDownFeeder.set(0);
+      arm4.set(0);
     }
+     
   }
 
   @Override
